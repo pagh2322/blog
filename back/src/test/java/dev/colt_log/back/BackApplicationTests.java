94eed05cb535b0cf588ce0f23a723f70d8a90409
Park Geonho <pagh2322@naver.com>
2024-08-31 02:55:25 +0900

    project init

---
+++ back/src/test/java/dev/colt_log/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.colt_log.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
